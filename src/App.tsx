346e09d69fc5807d387db2cc7244873788d42275
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2025-05-18 11:38:50 +0900

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState,useEffect } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-interface Station {
-  id: string;
-  numbering: string;
-  line: string;
-  name: string;
-  nameEnglish: string;
-  latitude: string;
-  longitude: string;
-  isFavorite: boolean;
-}
 
 async function load<T>(filePath: string): Promise<T> {
   try {
@@ -26,34 +15,7 @@ async function load<T>(filePath: string): Promise<T> {
 
 function App() {
   const [count, setCount] = useState(0);
-  const [stations, setStations] = useState<Station[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const stations = await load<Station[]>('stations_kobe_line.json');
-        setStations(stations);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-  
   return (
     <>
       <div>
@@ -76,20 +38,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    {/*}
-     <h2>神戸線</h2>
-      {stations.length > 0 ? (
-        <>
-          {stations.map((station) => (
-            <div key={station.id}>
-              <p>{station.name}({station.nameEnglish})</p>
-            </div>
-          ))}
-        </>
-      ) : (
-        <p>No stations data available.</p>
-      )}
-    { */ }
     </>
   )
 }
