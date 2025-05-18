e99cfa9b50dd3f8a4c8ca4a80adcc4a85f0c1499
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2025-05-18 23:55:12 +0900

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import StationCell from './StationCell';
 import useStationsData from './hooks/useStationsData'
