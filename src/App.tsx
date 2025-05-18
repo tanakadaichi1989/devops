0b1eaec27a50d27e793a1dac875ecf4fd676a973
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2025-05-18 11:43:51 +0900

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>This is edited on GitHub.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
